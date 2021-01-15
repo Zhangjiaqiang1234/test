d40d492eb2527642b00f51e226c89e414c8291d5
Zhangjiaqiang1234 <810751959@qq.com>
2021-01-15 12:10:20 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+
+        var box = document.getElementsByClassName('box')[0];
+
+        box.addEventListener('click', () => {
+                alert('测试页面')
+        })
+
+}
